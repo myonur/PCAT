f161df8cc53bb32e2603958b31422aeea7741d22
myonur <onur@uludag.edu.tr>
2024-09-25 11:42:59 +0300

    üçüncü kopyalama Kopyalama

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+
+app.set("view engine", 'ejs');
+
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+   res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+ })
+ app.get('/add', (req, res) => {
+    res.render('add');
+ })
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} nolu porttan çalışmaktadır`); 
+})
+
